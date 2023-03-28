58c9c5a8a9c85e6569540b6f76f6f6636ad6ed21
Vasil Lozanov <vasillozanov1@gmail.com>
2023-03-28 08:24:00 +0300

    extracted the genres panel

--- frontend/src/components/filter.tsx
+++ frontend/src/components/filter.tsx
@@ -1,21 +1,10 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, SetStateAction } from "react"
 import { HR, InputWithLabel, InputWithSuggestions } from "./common"
 import { Filters, SortingCriteria } from "./types"
 
 export function FilterSection({ filters, domain }: { filters: Filters, domain: string }) {
-    const [genres, setGenres] = useState<string[]>([])
     const [actors, setActors] = useState<string[]>([])
     const [directors, setDirectors] = useState<string[]>([])
-
-    useEffect(() => {
-        fetch(`${domain}/genres`).then((data) => {
-            return data.json()
-        }).then((newMovies) => {
-            setGenres(newMovies)
-        }).catch((err) => {
-            console.log(err)
-        })
-    }, [])
 
     useEffect(() => {
         const controller = new AbortController()
@@ -46,14 +35,6 @@ export function FilterSection({ filters, domain }: { filters: Filters, domain: s
     }, [filters.director])
 
 
-    function HandleSelectGenres(e: React.ChangeEvent<HTMLInputElement>) {
-        if (e.target.checked) {
-            filters.setSelectedGenres((g) => [...g, e.target.value])
-            return
-        }
-        filters.setSelectedGenres((g) => g.filter((v) => v != e.target.value))
-    }
-
     function HandleActorTextChange(e: any) {
         filters.setActor(e.target.value)
     }
@@ -63,15 +44,7 @@ export function FilterSection({ filters, domain }: { filters: Filters, domain: s
 
     return (
         <div className='filter grid-cont'>
-            <div className='grid-cont bg-2 shadowed w-90'>
-                <label className='text-header'>Жанрове (комбинирано)</label>
-                <HR />
-                <div className='grid-cont grid-cols-2 bg-2 pad-0 marg-0 justify-items-right'>
-                    {genres.map((val, idx) => {
-                        return <InputWithLabel labelVal={val} type="checkbox" key={idx} value={val} onChange={HandleSelectGenres} />
-                    })}
-                </div>
-            </div>
+            <GenresPanel domain={domain} filters={filters} />
             <div className='grid-cont bg-2 shadowed w-90 justify-items-right justify-content-space-around'>
                 <InputWithLabel labelVal='БГ Аудио' type='checkbox' checked={filters.bgAudio} onChange={(e: any) => filters.setBgAudio(e.target.checked)} />
                 <InputWithLabel labelVal='БГ Субтитри' type='checkbox' checked={filters.bgSubs} onChange={(e: any) => filters.setBgSubs(e.target.checked)} />
@@ -89,7 +62,41 @@ export function FilterSection({ filters, domain }: { filters: Filters, domain: s
     )
 }
 
-export function SortingPanel({ setSortCriteria }: any) {
+function GenresPanel({ domain, filters }: { domain: string, filters: Filters }) {
+    const [genres, setGenres] = useState<string[]>([])
+
+    useEffect(() => {
+        fetch(`${domain}/genres`).then((data) => {
+            return data.json()
+        }).then((newMovies) => {
+            setGenres(newMovies)
+        }).catch((err) => {
+            console.log(err)
+        })
+    }, [])
+
+    function HandleSelectGenres(e: React.ChangeEvent<HTMLInputElement>) {
+        if (e.target.checked) {
+            filters.setSelectedGenres((g) => [...g, e.target.value])
+            return
+        }
+        filters.setSelectedGenres((g) => g.filter((v) => v != e.target.value))
+    }
+
+    return (
+        <div className='grid-cont bg-2 shadowed w-90'>
+            <label className='text-header'>Жанрове (комбинирано)</label>
+            <HR />
+            <div className='grid-cont grid-cols-2 bg-2 pad-0 marg-0 justify-items-right'>
+                {genres.map((val, idx) => {
+                    return <InputWithLabel labelVal={val} type="checkbox" key={idx} value={val} onChange={HandleSelectGenres} />
+                })}
+            </div>
+        </div>
+    )
+}
+
+function SortingPanel({ setSortCriteria }: any) {
     function HandleChange(e: any) {
         setSortCriteria(e.target.value)
     }
