904cef3d666b180bc117002e2b05146fbd00ef4a
Vasil Lozanov <vasillozanov1@gmail.com>
2023-03-31 17:51:55 +0300

    subs and audio are now on the same row

--- frontend/src/components/filter.tsx
+++ frontend/src/components/filter.tsx
@@ -22,7 +22,7 @@ export function FilterSection({ domain, setFilterParams, hidden }: { setFilterPa
     return (
         <div className={`grid-cont filter ${hidden ? " hidden-sm" : ""}`}>
             <GenresPanel domain={domain} setSelectedGenres={setSelectedGenres} />
-            <div className='grid-cont bg-2 shadowed w-90-md justify-items-right justify-content-space-around'>
+            <div className='grid-cont grid-cols-2 bg-2 shadowed w-90-md justify-items-right justify-content-space-around'>
                 <InputWithLabel labelVal='БГ Аудио' type='checkbox' checked={bgAudio} onChange={(e: any) => setBgAudio(e.target.checked)} />
                 <InputWithLabel labelVal='БГ Субтитри' type='checkbox' checked={bgSubs} onChange={(e: any) => setBgSubs(e.target.checked)} />
             </div>
