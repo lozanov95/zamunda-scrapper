af1cc561bcfdcac04c2a56cf5996d2dfaaa8df46
Vasil Lozanov <vasillozanov1@gmail.com>
2023-05-05 18:51:26 +0300

    added script to scrape, copy the movies to the remote server and recreate the container

---
+++ copy-movies.ps1
@@ -0,0 +1,7 @@
+$KEY = "$Env:USERPROFILE\.ssh\ghdeploykey"
+$SERVERHOST = "ubuntu@vloz.website"
+$REMOTE_DIR = "{0}:{1}" -f $SERVERHOST, "zamunda-scrapper/movies.json"
+
+make scrape
+scp -i $KEY ./movies.json $REMOTE_DIR
+ssh -i $KEY $SERVERHOST "cd zamunda-scrapper && make prod"
