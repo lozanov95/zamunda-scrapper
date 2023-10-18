b027f42901bc40cca2d4230d250689e7c9f416e7
Vasil Lozanov <vasillozanov1@gmail.com>
2023-10-18 13:22:00 +0300

    styled the movie

--- frontend/src/components/movie.tsx
+++ frontend/src/components/movie.tsx
@@ -77,6 +77,7 @@ export const MoviesList = memo(function MoviesList({ movies, movieCount, setPage
 })
 
 export function Movie({ movie }: { movie: MovieType }) {
+    const [toggled, setToggled] = useState(false)
     const subs = useMemo(
         () => movie.torrents.find((el) => el.bg_subs === true) !== undefined,
         [movie.torrents]
@@ -86,16 +87,18 @@ export function Movie({ movie }: { movie: MovieType }) {
         [movie.torrents]
     )
 
+
+
     return (
-        <div className="flex flex-col lg:flex-row gap-2 bg-blue-100 py-4 px-4 border-2 border-blue-200 shadow-md shadow-blue-300 rounded-lg min-w-full hover:border-blue-400 hover:shadow-blue-400 cursor-pointer">
+        <div
+            className="flex flex-col lg:flex-row gap-2 bg-blue-100 py-4 px-4 border-2 border-blue-200 shadow-md shadow-blue-300 rounded-lg min-w-full hover:border-blue-400 hover:shadow-blue-400 cursor-pointer"
+            onClick={() => setToggled((value) => !value)}
+        >
             <div className="flex flex-col justify-items-center min-w-fit gap-1 place-items-center">
                 <div>
                     <MovieImage previewLink={movie.previewLink} />
                 </div>
-                <div className="font-bold text-center flex gap-1 items-center justify-center">
-                    <FontAwesomeIcon icon={faStar} className="text-yellow-500" />
-                    <span>{movie.rating}</span>
-                </div>
+                <Rating rating={movie.rating} />
                 <div className="flex gap-1 justify-center">
                     {subs && <div className="bg-teal-900 text-gray-200 shadow-lg px-2 py-1 rounded-lg font-semibold text-center">БГ Суб</div>}
                     {audio && <div className="bg-lime-900 text-gray-200 shadow-lg px-2 py-1 rounded-lg font-semibold text-center">БГ Аудио</div>}
@@ -105,15 +108,37 @@ export function Movie({ movie }: { movie: MovieType }) {
                 <div className="font-semibold">{movie.title}</div>
                 <div className="text-sm text-gray-500 font-semibold">{movie.year}{movie.genres.length > 0 && ` - ${movie.genres.join(", ")}`} </div>
                 <div className="border-t-2 border-blue-200 indent-3">{movie.description}</div>
+                <div className={`${toggled ? "flex flex-col" : "hidden"} border-t-2 border-blue-200`}>
+                    <div className="flex flex-col gap-2">
+                        {movie.directors.length > 0 && <div className="flex text-sm text-gray-500 font-bold">Режисиран от {movie.directors.join(", ")}</div>}
+                        {movie.actors.length > 0 && <div className="flex text-sm text-gray-500 font-bold">С участието на {movie.actors.join(", ")}</div>}
+                    </div>
+                    <div className="flex flex-wrap gap-2 justify-center">
+                        {movie.torrents.map((torrent, i) => <Torrent torrent={torrent} key={i} />)}
+                    </div>
+                </div>
             </div>
         </div>
     )
 }
 
-function MovieDetailed({ movie }: { movie: MovieType }) {
+function Torrent({ torrent }: { torrent: TorrentType }) {
     return (
-        <div>
-
+        <div
+            className="bg-blue-300 max-w-fit px-4 py-2 rounded-lg place-items-center flex flex-col cursor-pointer border-2 border-blue-300 hover:border-blue-400 shadow-sm shadow-blue-500"
+            onClick={() => { window.open(`https://zamunda.net${torrent.link}`) }}
+        >
+            <div className="flex flex-col gap-2">
+                {!torrent.bg_audio &&
+                    <div className="bg-lime-900 text-gray-200 shadow-sm shadow-teal-800 px-2 py-1 rounded-lg font-semibold text-center">БГ Аудио</div>
+                }
+                {torrent.bg_subs &&
+                    <div className="bg-teal-900 text-gray-200 shadow-sm shadow-teal-800 px-2 py-1 rounded-lg font-semibold text-center">БГ Субс</div>
+                }
+            </div>
+            <div className="text-sm font-semibold text-gray-600">
+                {torrent.size}
+            </div>
         </div>
     )
 }
@@ -125,5 +150,14 @@ function MovieImage({ previewLink }: { previewLink: string }) {
 
     return (
         <img src={src} className="max-w-[200px] max-h-[200px] rounded-lg" />
+    )
+}
+
+function Rating({ rating }: { rating: string | number }) {
+    return (
+        <div className="font-bold text-center flex gap-1 items-center justify-center">
+            <FontAwesomeIcon icon={faStar} className="text-yellow-500" />
+            <span>{rating}</span>
+        </div>
     )
 }
